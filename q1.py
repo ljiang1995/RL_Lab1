efd7f89e4655d4abeed2d2a35392c15e01ae592d
tanlunte <45292065+tanlunte@users.noreply.github.com>
2018-11-23 18:35:00 +0100

    Update q1.py

--- q1.py
+++ q1.py
@@ -1,4 +1,9 @@
-def bellman(p,m,action):#e.g.: p(3,4) m(4,3) action(-1,0).
-  
-  
+availble_choice=5
+moves=[[-1 0],[1 0], [0,1], [0,-1], [0 0]]
+def bellman(p,m,u,action):#e.g.: p(3,4) m(4,3) action(-1,0).
+  for i in range(available_choice):
+    for r in range(2):
+    temp1=p
+    temp1[i][j]=
+
   return probability
