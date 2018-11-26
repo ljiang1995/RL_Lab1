0686820ce6cce3e07b13c8b7e63b6989d07361c7
tanlunte <45292065+tanlunte@users.noreply.github.com>
2018-11-26 12:07:36 +0100

    Update q1.py

--- q1.py
+++ q1.py
@@ -1,8 +1,12 @@
+import numpy as np
+Max_T=15
+u=np.zeros((Max_T+1,902))
 available_choice=4
 moves=[[-1, 0],[1,0], [0,1], [0,-1], [0, 0]]
 numsList=[]
-def bellman(m,action):#e.g.:m(4,3) action(-1,0).
+def bellman(m):#e.g.:m(4,3) action(-1,0).
   cnt=0
+  s=[]
   for row in range(4):
         numsList.append([])
         for column in range(2):
@@ -13,6 +17,26 @@ def bellman(m,action):#e.g.:m(4,3) action(-1,0).
    	    numsList[i][j]=m[j]
 	    numsList[i][j]+=moves[i][j]
       if (numsList[i][0] in range(5)) and (numsList[i][1] in range(6)):	
-	    cnt+=1
+	    s.append(numsList[i])
+            cnt+=1
   probability=1.0/cnt  
-  return probability
+  return probability,s
+
+def reward(T, state):
+	if T==Max_T:
+		if state==900:# state fail
+			u[T,state]=0
+		elif (state>=840) and (state!=840+24+4):
+			u[T,state]=1
+	else:
+		if state==900:
+			u[T,state]=0
+		elif state==901:
+			u[T,state]=1
+		else:
+			temp=state%30
+			c=temp//6
+			d=temp%6
+			bellman(m)
+			
+		
