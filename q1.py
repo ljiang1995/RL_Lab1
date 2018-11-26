6058d3922661844486652931e32c8cba7b8a84d5
tanlunte <45292065+tanlunte@users.noreply.github.com>
2018-11-26 07:01:11 +0100

    Update q1.py

--- q1.py
+++ q1.py
@@ -1,9 +1,20 @@
-availble_choice=5
-moves=[[-1 0],[1 0], [0,1], [0,-1], [0 0]]
-def bellman(p,m,u,action):#e.g.: p(3,4) m(4,3) action(-1,0).
+available_choice=4
+moves=[[-1, 0],[1,0], [0,1], [0,-1], [0, 0]]
+numsList=[]
+def bellman(p,m,action):#e.g.: p(3,4) m(4,3) action(-1,0).
+  for row in range(4):
+        numsList.append([])
+        for column in range(2):
+          num =0
+          numsList[row].append(num)
+  print numsList
   for i in range(available_choice):
-    for r in range(2):
-    temp1=p
-    temp1[i][j]=
+      print('temp1[i][j] is',numsList,p)
+      for j in range(2):
+   	  numsList[i][j]=p[j]
+	  	numsList[i][j]+=moves[i][j]
+		
+  probability=1
+ 
 
   return probability
