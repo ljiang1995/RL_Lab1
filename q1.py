e0f50baf7ddce3f6d70050f439a8152f3df1066d
tanlunte <45292065+tanlunte@users.noreply.github.com>
2018-11-26 18:34:22 +0100

    Update q1.py

--- q1.py
+++ q1.py
@@ -22,6 +22,16 @@ def bellman(m):#e.g.:m(4,3)
   probability=1.0/cnt  
   return probability,s
 
+def player_move(p):
+	possible_move=[]
+	player=[[0,0],[0,0],[0,0],[0,0],[0,0]]
+	for i in range(5):
+		for j in range(2):
+			player[i][j]+=p[j]
+			player[i][j]+=moves[i][j]
+		if (player[i][0] in range(6)) and (player[i][1] in range(5)):	
+				possible_move.append(moves[i])
+	return possible_move
 def get_point(s):
 	temp=s//30
 	a=temp%6
@@ -31,7 +41,7 @@ def get_point(s):
 	d=temp//6
 	return a,b,c,d	
 def get_state(a,b,s):
-	return 30*(6*a+b)+6*s[0]+s[1]
+	return 30*(a+6*b)+s[0]+6*s[1]
 def reward(T, state, action):
 	probability=0
 	s=[]
@@ -43,6 +53,8 @@ def reward(T, state, action):
 		elif (a==4 and b==4) and (state!=868): #state 868 is the state that player and minotaur are both at (4,4)
 			u[T,state]=1
 			return 1
+		else:
+			return 0
 	else:
 		a,b,c,d=get_point(state)
 		if state==900:
@@ -54,9 +66,22 @@ def reward(T, state, action):
 		else:
 			a+=action[0]
 			b+=action[1]
+			if (a==4 and b==4) and (state!=868):
+				return 1
+			elif (a==4 and b==4) and (state==868):
+				return 0
 			probability,s=bellman([c,d])
-			print s
-			for i in range(len(s)):
-				u[T,state]+=probability*u[T+1,get_state(a,b,s[i])]
-			
-reward(14,872,[-1,0])
+			possible_move=player_move([a,b])
+			print possible_move
+			best_result=0
+			for i in range(len(possible_move)):
+				temp_result=0
+				for j in range(len(s)):
+					temp_result+=probability*reward(T+1,get_state(a,b,s[j]),possible_move[i])
+					print 'T= ',T,'j= ',j,'a,b,c,d= ',a,b,c,d,'temp_result= ',temp_result		
+				if temp_result>=best_result:
+					best_result=temp_result
+					best_action=possible_move[i]
+			u[T,state]=best_result
+			return u[T,state]
+print reward(0,28,[0,0])
